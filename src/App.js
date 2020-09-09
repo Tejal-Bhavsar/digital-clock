6cc2d9a2e0f14c41000191478d0cd040e501f98e
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-09-09 12:46:00 +0530

    used hook for state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+ 
+ 
 
-function App() {
+const  App = () => {
+
+  let time = new Date().toLocaleTimeString();
+   
+  const [cnttime, updatetime] = useState(time);
+
+   const myTime  = () => {
+     time = new Date().toLocaleTimeString();
+       updatetime(time);
+
+     }
+
+  setInterval (myTime,1000)
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="time"> 
+     <h1> Digital Clock</h1>
+     <h1> {cnttime} </h1>
+     </div>
+     </>
   );
 }
 
